3ba657624a0b1b7a9b7013b283a62c62e06c0f8e
maks <maks-lapin-1998@.ru>
2023-05-06 21:02:38 +0400

    task52

---
+++ task52/Program.cs
@@ -0,0 +1,42 @@
+﻿int[,] FillFrrayMatrix(int[,] matr) // заполняет матрицу случайными числами
+{
+    Random rnd =new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = rnd.Next(0, 2);
+        }
+    }
+    return matr;
+}
+void PrintArrayMatrix(int[,] matr)  // выводит матрицу на экран
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+void SrAr(int[,] matr)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        int sum = 0;
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            sum += matr[i,j];
+        }
+        double srar = sum / (double)matr.GetLength(0);
+        System.Console.Write($"({Math.Round(srar, 1)}) ");
+    }
+}
+
+int[,] matrix = new int[2, 5];
+matrix = FillFrrayMatrix(matrix);
+PrintArrayMatrix(matrix);
+System.Console.WriteLine();
+SrAr(matrix);
