3c4c3a0b5585bc20843f69765e6518ac746a71e7
maks <maks-lapin-1998@.ru>
2023-05-06 20:49:57 +0400

    task47

---
+++ task47/Program.cs
@@ -0,0 +1,45 @@
+﻿int ReadInt(string argument) 
+{
+    Console.Write(argument);
+    int i;
+    while (!int.TryParse(Console.ReadLine(), out i))
+    {
+        System.Console.WriteLine("Это не число!");
+        Console.Write(argument);
+    }
+    return i;
+}
+
+double[,] FillFrrayMatrix(double[,] matr) 
+{
+    Random rnd =new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = Math.Round(rnd.Next(0, 10) + rnd.NextDouble(), 2);
+        }
+    }
+    return matr;
+}
+
+void PrintArrayMatrix(double[,] matr)  
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+
+
+int m = ReadInt("Введите количество столбцов ");
+int n = ReadInt("Введите количество строк ");
+double[,] matrix = new double[m ,n];
+FillFrrayMatrix(matrix);
+PrintArrayMatrix(matrix);
